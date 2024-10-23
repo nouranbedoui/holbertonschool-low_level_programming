98469c20a8f361ad05ea1b0986b0eeae38c3d1eb
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-10-23 08:43:48 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo '#include <stdio.h> int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }'
+#include <stdio.h> int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }
