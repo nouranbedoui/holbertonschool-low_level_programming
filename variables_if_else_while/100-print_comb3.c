275a368f44ad38a4c389a455421ab474a9db42a1
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-10-27 21:04:23 +0100

    printc

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -2,22 +2,22 @@
 
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');  /* Print first digit */
-            putchar(j + '0');  /* Print second digit */
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');  /* Print first digit */
+			putchar(j + '0');  /* Print second digit */
 
-            if (i != 8 || j != 9)  /* Avoid printing comma and space after the last combination */
-            {
-                putchar(',');  /* Print comma */
-                putchar(' ');  /* Print space */
-            }
-        }
-    }
-    putchar('\n');  /* Print newline at the end */
-    return (0);
+			if (i != 8 || j != 9)  /* Avoid printing comma and space after the last combination */
+			{
+				putchar(',');  /* Print comma */
+				putchar(' ');  /* Print space */
+			}
+		}
+	}
+	putchar('\n');  /* Print newline at the end */
+	return (0);
 }
