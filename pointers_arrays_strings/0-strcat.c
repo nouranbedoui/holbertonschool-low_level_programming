b3b45a690b187c558787a66b0614e54b07de8525
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-03 12:22:29 +0100

    stractat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string to which src will be appended.
+ * @src: The source string to append to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+char *ptr = dest;
+
+while (*ptr != '\0')
+{
+ptr++;
+}
+
+
+while (*src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+}
+
+
+*ptr = '\0';
+
+return (dest);
+}
