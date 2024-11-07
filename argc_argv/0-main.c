8eb01c8553b31894e4d52fafd80be9df59aab337
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-07 12:54:35 +0100

    what

--- argc_argv/0-main.c
+++ argc_argv/0-main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include "main.h"  // Include the header file
 
-/* main function */
 int main(int argc, char *argv[])
 {
-    /* Print the name of the program (argv[0]) */
-    printf("%s\n", argv[0]);
-
-    return 0;
+/* Print the name of the program, which is stored in argv[0] */
+printf("%s\n", argv[0]);
+return (0);
 }
