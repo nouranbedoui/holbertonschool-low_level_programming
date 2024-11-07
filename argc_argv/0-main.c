ba7961235c21d5d150c0459dd7b5ad571873dd10
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-07 13:12:36 +0100

    new

--- argc_argv/0-main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-/* Print the name of the program, which is stored in argv[0] */
-printf("%s\n", argv[0]);
-return (0);
-}
--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the program followed by a new line
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-
 (void)argc;
-
-
 printf("%s\n", argv[0]);
-
 return (0);
 }
-
