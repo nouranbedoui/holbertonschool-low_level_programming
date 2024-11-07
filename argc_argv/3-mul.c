8f788a63629fb183d5ad5eadea2b15d2ca1d29d2
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-07 13:19:32 +0100

    nul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers and prints the result
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 if successful, 1 if error (not exactly two arguments)
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1  *num2;
+
+printf("%d\n", result);
+
+return (0);
+}
