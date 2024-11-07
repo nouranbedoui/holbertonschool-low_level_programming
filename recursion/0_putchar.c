7b4363ac9041bc159d41ebc19f6341632203ffed
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-07 11:13:17 +0100

    alltasks

---
+++ recursion/0_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void _putchar(char c)
+{
+    putchar(c); 
+}
+
---
+++ recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ *
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+void _putchar(char c)
+{
+    putchar(c);
+}
+
