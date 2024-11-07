e8abfe4dd2a23bfe8400c9119539a98deeb18e23
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-07 11:23:11 +0100

    alltask

--- recursion/main.h
+++ recursion/main.h
@@ -4,7 +4,9 @@
 #include <stdio.h> 
 
 void _puts_recursion(char *s);
-void _putchar(char c);  
+void _putchar(char c); 
+void _print_rev_recursion(char *s);
+void _putchar(char c);
 
 #endif /* MAIN_H */
 
