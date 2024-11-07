78d9be35126911561f02d26497ffca4dd00565db
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-07 12:49:15 +0100

    what

---
+++ argc_argv/0-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"  // Include the header file
+
+/* main function */
+int main(int argc, char *argv[])
+{
+    /* Print the name of the program (argv[0]) */
+    printf("%s\n", argv[0]);
+
+    return 0;
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype */
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
+
