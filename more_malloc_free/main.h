630eebdde500bbf8f680edf0f347be783ff221ad
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-15 19:26:56 +0100

    mallooc

---
+++ more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void simple_print_buffer(int *buffer, unsigned int size);
+int *array_range(int min, int max);
