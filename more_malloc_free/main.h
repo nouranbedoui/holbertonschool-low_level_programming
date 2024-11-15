1e7fd67b4ca02df91c732fb8ad56d87f78e0f659
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-15 19:36:04 +0100

    malloc

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,9 +1,13 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
+#include <stdio.h>
 #include <limits.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
-void simple_print_buffer(int *buffer, unsigned int size);
 int *array_range(int min, int max);
+
+#endif
