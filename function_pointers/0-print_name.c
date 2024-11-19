37f005e0b16c66c8723d16bab8cf59520e64706c
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-19 10:30:50 +0100

    fun

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,53 +1,14 @@
-#include "main.h"
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using the function pointed to by f.
- * @name: The name to be printed.
- * @f: The function pointer that will be used to print the name.
+ * print_name - prints a name using the function passed as a pointer
+ * @name: name of the person
+ * @f: pointer to a function that takes a name and prints it
  *
  * Return: Nothing.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
 f(name);
-}
-
-/**
- * print_name_as_is - prints a name as it is.
- * @name: The name to be printed.
- *
- * Return: Nothing.
- */
-void print_name_as_is(char *name)
-{
-printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - prints a name in uppercase.
- * @name: The name to be printed.
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-unsigned int i = 0;
-
-printf("Hello, my uppercase name is ");
-while (name[i])
-{
-if (name[i] >= 'a' && name[i] <= 'z')
-{
-putchar(name[i] - 'a' + 'A');
-}
-else
-{
-putchar(name[i]);
-}
-i++;
-}
-putchar('\n');
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+
 void print_name(char *name, void (*f)(char *));
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
-
