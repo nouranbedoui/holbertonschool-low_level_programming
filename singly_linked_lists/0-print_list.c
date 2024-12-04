ac87fb7aa120a90c8f9d17d71d2f06ea3b37e245
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-12-04 21:07:01 +0100

    fixedfiles

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,11 +9,10 @@
 size_t print_list(const list_t *h)
 {
 size_t i = 0;
-
 while (h != NULL)
 {
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
+    if (h->str == NULL)
+    printf("[0] (nil)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
