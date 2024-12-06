122b2efacb41a74941e405347ce9b300425cb6fc
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-12-06 13:51:44 +0100

    prinddlistint

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t count = 0;
+const dlistint_t *current = h;
+
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+count++;
+}
+
+return (count);
+}
